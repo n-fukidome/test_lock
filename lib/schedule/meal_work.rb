7b52238a4dc5aa0a8bd8e7b934a3172c51929e2f
n-fukidome <n-fukidome@nekojarashi.com>
2016-01-13 17:56:05 +0900

    fix for rails console

--- lib/schedule/meal_work.rb
+++ lib/schedule/meal_work.rb
@@ -9,18 +9,20 @@ module Schedule
     end
 
     private
-    def _trigger_all
+    def self._trigger_all
       Species.all.each do |species|
-        puts 'start each species'
-        @breeder = @species.breeders.last
-        next unless @breeder # next になることはないんはず
+        begin
+          puts 'start each species'
+          @breeder = species.breeders.last
+          next unless @breeder # next になることはないんはず
 
-        activation_until = species.animals.first.creature.activation_until
-        next unless is_alive? activation_until
-        yield species
-      rescue => e
-        puts '==================error!=================='
-        puts e.message
+          activation_until = species.animals.first.creature.activation_until
+          next unless _is_alive? activation_until
+          yield species
+        rescue => e
+          puts '==================error!=================='
+          puts e.message
+        end
       end
     end
 
@@ -39,7 +41,7 @@ module Schedule
     end
 
     # アカウントが生きてるかどうかの判定(今回は基本関係ない)
-    def _self.is_alive?(activation_until)
+    def self._is_alive?(activation_until)
       activation_until.localtime < DateTime.now.end_of_day && activation_until.localtime > DateTime.now
     end
   end
